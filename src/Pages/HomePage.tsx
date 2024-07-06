10ff3e6b8577625ca27fc323b9222015ae4b3d1c
wtmsi <91251429+marketpanda@users.noreply.github.com>
2024-07-07 07:58:26 +0800

    removed unused

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,57 +1,18 @@
 import { Card } from "../components/ui/card" 
-import * as Form from '@radix-ui/react-form' 
-import { useEffect, useState } from "react"
-import { supabase } from '../client'
-import FooterGuest from "./FooterGuest"
+
+import { useEffect } from "react"
+
 import { Link, useNavigate } from "react-router-dom"
 import { Props } from "./DashBoardAuthenticated"
 
 
 const HomePage:React.FC<Props> = ({ setToken }) => {
 
-    type SignInDetails = {
-        email: string | null,
-        password: string | null, 
-    }
+    console.log(setToken)
 
     let navigate = useNavigate()
 
-    const [formDataSignIn, setFormDataSignIn] = useState<SignInDetails>({
-        email: null,
-        password: null,
-    })
-
-    const handleChangeSignInForm:React.ChangeEventHandler<HTMLInputElement> = async(event) => { 
-        setFormDataSignIn((prev) => {
-            return {
-                ...prev, [event?.target.name]: event?.target.value
-            }
-        })
-    }
-
-    const handleLogin = async(event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-        const { email, password } = formDataSignIn
-        if (email && password) {
-            try {
-              const { data, error } = await supabase.auth.signInWithPassword({
-                email, password
-              })  
-
-              if (error) throw error
-              console.log('received ', data )
-              if (setToken) setToken(JSON.stringify(data))
-              navigate('/dashboard')
-              
-            } catch (error) {
-                console.log(error)
-            }
-
-            console.log(email, password)
-        } else {
-            console.log("Email and password is required")
-        }
-    }   
+          
 
     useEffect(() => {
         const tokenString = sessionStorage.getItem('token')
